a7bb99003bce62e1328dbd773c7f17cf1cb101ea
Kuldeepagrahari <kuldeepagrahari9103@gmail.com>
2023-12-25 18:41:36 +0530

    api postgreSql

---
+++ Axios/solution.js
@@ -0,0 +1,54 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* This code block is defining a route handler for
+ the GET request to the root URL ("/"). */
+/* The code block you provided is defining two route handlers for the root URL ("/") - one for GET
+requests and one for POST requests. */
+app.get("/", async (req, res) => {
+  res.render("solution.ejs")
+/* This code block is handling a GET request to the root URL ("/"). */
+  // try {
+  //   const response = await axios.get("https://bored-api.appbrewery.com/random");
+  //   const result = response.data;
+  //   console.log(result);
+  //   res.render("solution.ejs", { data: result });
+  // } catch (error) {
+  //   console.error("Failed to make request:", error.message);
+  //   res.render("solution.ejs", {
+  //     error: error.message,
+  //   });
+  // }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const type = req.body.type;
+    const participants = req.body.participants;
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`
+    );
+    const result = response.data;
+    console.log(result);
+    res.render("solution.ejs", {
+      data: result[Math.floor(Math.random() * result.length)],
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("solution.ejs", {
+      error: "No activities that match your criteria.",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
